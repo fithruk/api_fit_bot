62d84e5e0120fda6f82109179f26ce3c3d383e0a
Sergey Belov <sergeytrenerbelov@gmail.com>
2023-10-13 11:21:14 +0300

    112

--- routes/exerciseRoute.js
+++ routes/exerciseRoute.js
@@ -7,11 +7,8 @@ const router = Router();
 
 // удалить потом нижестоящий код
 router.get("/", async (req, res) => {
-  const subDirectory = "arms"; // часть пути ,категория группы упражнений, должна приходить из запроса
   try {
-    const requestedGroupe = await allExerciseService.getAllExercises(
-      subDirectory
-    );
+    const requestedGroupe = await allExerciseService.getAllMusclesGroups();
 
     res.setHeader("Content-Type", "application/json");
     res.json(requestedGroupe);
