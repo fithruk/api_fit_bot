ed7e63947407ed83ee01aa1901989fb219d6f2e1
Sergey Belov <sergeytrenerbelov@gmail.com>
2024-04-15 12:13:59 +0300

    asd

--- models/statModel.js
+++ models/statModel.js
@@ -1,7 +1,26 @@
 const { Schema, model } = require("mongoose");
 // Релизовать функционал сохранения прогресса пользователя
 const StatisticsShema = new Schema({
-  absoluteRecords: {},
+  userName: {
+    type: String,
+    requaired: true,
+  },
+  absoluteRecords: [
+    {
+      exersice: {
+        type: String,
+      },
+      weight: {
+        type: Number,
+      },
+      countOfReps: {
+        type: Number,
+      },
+      date: {
+        type: Date,
+      },
+    },
+  ],
 });
 
 module.exports = model("StatisticsShema", StatisticsShema);
